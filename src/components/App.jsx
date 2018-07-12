b0c6c70856609e0056bb01e58bf862c55d3745c2
Eva Antipina <eva.antipina@gmail.com>
2018-07-12 14:22:16 -0700

    Initial project set up.

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Button } from 'reactstrap';
+
+function App(){
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <Switch>
+
+      </Switch>
+      <hr/>
+      <Button color="danger">Danger!</Button>
+    </div>
+  );
+}
+
+export default App;
