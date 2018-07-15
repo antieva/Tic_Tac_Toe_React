4fdd60ed5632e5326f1dec1ae5d267ac8a5783c2
Eva Antipina <eva.antipina@gmail.com>
2018-07-15 10:17:48 -0700

    Add xIsNext state.

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
-    return (
-      <div
-         className="square"
-         onClick={() => this.setState({value: 'X'})}>
-        <style jsx>{`
+function Square(props) {
+  return (
+    <div
+      className="square"
+      onClick={props.onClick}>
+      <style jsx>{`
           .square {
             width: 100px;
             height: 105px;
             border: 1px solid green;
             padding: 44px;
           }
-      `}</style>
-    {this.state.value}
-  </div>
-    );
-  }
+        `}</style>
+      {props.value}
+    </div>
+  );
 }
 
 export default Square;
